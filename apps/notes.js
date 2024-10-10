71966774d3ecaffb505e4f4b46a7a5846584d044
Syrious <syrious@gmx.net>
2024-10-10 08:38:30 +0200

    Adds dialog

---
+++ apps/notes.js
@@ -0,0 +1,26 @@
+export class Notes extends Application {
+    constructor(object, options = {}) {
+        super(object, options);
+    }
+
+    static get defaultOptions() {
+        return foundry.utils.mergeObject(super.defaultOptions, {
+            id: "notes",
+            title: "Thank you for downloading",
+            template: "modules/kctg-a5e/templates/notes.html",
+            width: 800,
+            height: 550
+        });
+    }
+
+    activateListeners(html) {
+        super.activateListeners(html);
+        $('.clear', html).bind("click", () => {
+            game.settings.set("kctg-1e", "show-warning", false);
+            this.close();
+        });
+        $('.just-close', html).bind("click", () => {
+            this.close();
+        });
+    }
+}
