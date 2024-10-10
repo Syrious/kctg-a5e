013611f131723b11f7953db8c15a5e88eba3b312
Syrious <syrious@gmx.net>
2024-10-10 10:02:22 +0200

    Prevent popup if kctg-5e is active

--- kctg-a5e.js
+++ kctg-a5e.js
@@ -4,6 +4,10 @@ export const MODULE_ID = 'kctg-a5e';
 
 // Register the 'show-warning' setting
 Hooks.once('init', () => {
+    if(isKCTG5eActive()){
+        return;
+    }
+
     game.settings.register(MODULE_ID, 'show-warning', {
         name: 'Show "Thank you" note on startup',
         scope: 'world',
@@ -18,9 +22,14 @@ function setting(key) {
     return game.settings.get(MODULE_ID, key);
 }
 
+function isKCTG5eActive() {
+    let kctg5eIsActive = game.modules.has("kctg-5e") && game.modules.get("kctg-5e").active
+    return kctg5eIsActive;
+}
+
 // Ensure the 'setting' function is defined and available in scope
 Hooks.once('ready', () => {
-    if (setting("show-warning") && game.user?.isGM) {
+    if (setting("show-warning") && game.user?.isGM && !isKCTG5eActive()) {
         new Notes().render(true);
     }
 });
